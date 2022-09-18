50944b3bce7997d6b968d4ea0f19f098d79d5e9c
KseniaPar <k.parpulova@gmail.com>
2022-09-19 03:29:05 +0500

    Add Lazy fetch

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -32,7 +32,8 @@ public class User implements UserDetails {
     @Column(name = "email")
     private String email;
 
-    @ManyToMany()
+
+    @ManyToMany(fetch=FetchType.LAZY)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
